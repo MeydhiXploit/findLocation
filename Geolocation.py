c3679be5d6c7c2e8c7172674b1a8c48a4dd4caa6
MeydhiXploit <meydhimeydhi@gmail.com>
2023-06-14 21:01:03 -0700

    Update README.md

--- Geolocation.py
+++ Geolocation.py
@@ -1,12 +1,13 @@
 import requests
 import json
 import cowsay
+
 cowsay.cow("MeydhiXploit")
 print("-" * 40)
 ipaddress = input("Input Your IP Address: ")
 iprequests = requests.get(f"http://ip-api.com/json/{ipaddress}")
 if iprequests.status_code == 200:
-    ipdata = iprequests.json()
+    ipdata = json.loads(iprequests.text)
     if ipdata["status"] == "success":
         print("Ip address:", ipaddress)
         print("Query:", ipdata["query"])
@@ -14,13 +15,14 @@ if iprequests.status_code == 200:
         print("Country Code:", ipdata["countryCode"])
         print("City:", ipdata["city"])
         print("Timezone:", ipdata["timezone"])
-        print("Status:", ipdata["status"])
         print("Region:", ipdata["region"])
         print("Region name:", ipdata["regionName"])
-        print("Lat :",ipdata["lat"])
-        print("Lon :",ipdata["lon"])
+        lat = ipdata["lat"]
+        lon = ipdata["lon"]
+        print("Location :",lat,",",lon)
+        maps = f"https://www.google.com/maps/@{lat},{lon},9z?hl=id"
+        print("Maps :",maps)
+        print("Status:", ipdata["status"])
         print("-" * 40)
     else:
         print("Failed to retrieve IP information.")
-else:
-    print("Failed to connect to IP API.")
